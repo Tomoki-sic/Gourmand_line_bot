10b0bde5a3b9b631ed127c637e5a933739856265
Tomoki-sic <cy17237@shibaura-it.ac.jp>
2021-12-23 18:43:54 +0900

    test

--- manage.py
+++ manage.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "line_bot.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
     from django.core.management import execute_from_command_line
 
