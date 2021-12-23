8033a11d98fcca8ce766a5c21877eed5b97988a1
Tomoki-sic <cy17237@shibaura-it.ac.jp>
2021-12-23 19:40:56 +0900

    d

--- line_bot_ai/views.py
+++ line_bot_ai/views.py
@@ -14,7 +14,7 @@ def message(request):
         data = request['events'][0]
         message = data['message']
         reply_token = data['replyToken']
-        line_message = LineMessage(message_creater.create_single_text_message(message['text']))
+        line_message = LineMessage(message['text'])
         line_message.reply(reply_token)
         return HttpResponse("ok")
 
