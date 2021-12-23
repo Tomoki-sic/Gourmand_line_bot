c5697f3ba697ea64acb2d3779d8c7ef431d53099
Tomoki-sic <cy17237@shibaura-it.ac.jp>
2021-12-23 16:28:17 +0900

    Update settings.py

--- line_bot/settings.py
+++ line_bot/settings.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import django_heroku
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,4 +125,5 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+# Activate Django-Heroku.
+django_heroku.settings(locals())
