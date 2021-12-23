d4b3c239258f2e3adfa7c8fa4bffb9ad4b9e8eec
Tomoki-sic <cy17237@shibaura-it.ac.jp>
2021-12-23 16:14:02 +0900

    ｓ

--- line_bot_ai/views.py
+++ line_bot_ai/views.py
@@ -12,10 +12,9 @@ from line_bot_ai.line_message import LineMessage
 def index(request):
     if request.method == 'POST':
         request = json.loads(request.body.decode('utf-8'))
-        if len(request['events'][0]) != 0:
-            data = request['events'][0]
-            message = data['message']
-            reply_token = data['replyToken']
-            line_message = LineMessage(message_creater.create_single_text_message(message['text']))
-            line_message.reply(reply_token)
+        data = request['events'][0]
+        message = data['message']
+        reply_token = data['replyToken']
+        line_message = LineMessage(message_creater.create_single_text_message(message['text']))
+        line_message.reply(reply_token)
         return HttpResponse("ok")
